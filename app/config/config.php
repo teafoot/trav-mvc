83834872d583193d5a60ec15fe91f353ca1fc7c3
teafoot <davidchenlog@gmail.com>
2020-01-12 16:44:44 -0800

    finished project

---
+++ app/config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+define("DB_HOST", "localhost");
+define("DB_USER", "");
+define("DB_PASS", "");
+define("DB_NAME", "");
+
+
+define("APPROOT", dirname(dirname(__FILE__)));
+define("URLROOT", "");
+define("SITENAME", "");
